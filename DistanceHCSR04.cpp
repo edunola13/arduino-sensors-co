6b7e18ef51787853409fc47491bbe0068bec86ee
edunola13 <edunola13@hotmail.com>
2019-06-26 19:59:18 -0300

    Update DistanceHCSR04.cpp

--- DistanceHCSR04.cpp
+++ DistanceHCSR04.cpp
@@ -1,7 +1,8 @@
 //#include "WProgram.h"
 #include <Arduino.h>
 //#include "vendor/igniteit/arduino-basic-oo/ElementAbstract.h"
-#include "../arduino-basic-oo/ElementAbstract.h"
+//#include "../arduino-basic-oo/ElementAbstract.h"
+#include <ElementAbstract.h>
 #include "DistanceHCSR04.h"
 
 DistanceHCSR04::DistanceHCSR04() {
